22683ace4b63a81e6a519802d73e7ed6e6f67d55
tBaronDar <thedarelis@gmail.com>
2024-12-06 11:13:54 +0200

    Add project files.

---
+++ Polymorphism-Interfaces/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Polymorphism_Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
